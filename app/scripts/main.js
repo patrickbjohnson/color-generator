61d0549cbe2cc0a703b9272d04674dae9b305805
Patrick Johnson <pjohnson@ADMC82A141DEEFE.local>
2014-11-26 14:19:14 -0500

    base js

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,16 @@
-console.log('\'Allo \'Allo!');
+'use strict';
+$(document).ready(function(){
+  
+  function colorize(el){
+      var randomColor = Math.floor(Math.random()*16777215).toString(16);
+      $(el).css({
+      'background-color': '#' + randomColor
+      });
+      console.log($(el).css('background-color'));
+  }
+
+  $('.colorize').on('click', function(e){
+    e.preventDefault();
+    colorize(this);
+  });
+});
